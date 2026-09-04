714ad44315cb4a9448bed266183930cc860c56ae
Rorasbd <oreoreggae@gmail.com>
2026-09-04 13:13:59 +0700

    Create server.js

---
+++ server.js
@@ -0,0 +1,28 @@
+const http = require("http");
+const fs = require("fs");
+const path = require("path");
+
+const PORT = process.env.PORT || 3000;
+const HOST = "0.0.0.0";
+
+const server = http.createServer((req, res) => {
+  const filePath = path.join(__dirname, "index.html");
+
+  fs.readFile(filePath, (err, data) => {
+    if (err) {
+      res.writeHead(500);
+      res.end("Server Error");
+      return;
+    }
+
+    res.writeHead(200, {
+      "Content-Type": "text/html; charset=utf-8"
+    });
+
+    res.end(data);
+  });
+});
+
+server.listen(PORT, HOST, () => {
+  console.log(`Server running on ${HOST}:${PORT}`);
+});
